f3043a7253fe6df4ca0e696f851e2ff28a189e1c
ValikSk8er <valiksk8@gmail.com>
2018-09-22 20:13:55 +0300

    doBinarySearch is finished

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,34 +1,30 @@
-import java.util.Arrays;
 
 public class BinarySearch {
 
-//  [5, 6, 9, 10, 15, 1, 3, 4]. It means you should modify basic implementation of the BinarySearch.
-//          ***WARNING!
-//          If you have this kind of case 5,5,1,3,4,5,5,5,5, and your target
-// is 5 result value should be index of first satisfactory value in array. In our array it will be 0
-
-    public static void main(String[] args) {
-
-        int[] values = {1, 2, 4, 6, 7, 11, 12, 14, 15, 16, 22, 23, 54, 55, 67, 89, 90, 91 ,92};
-        int target = 11;
-
-        System.out.println(doBinarySearch(values, target, 0, values.length - 1));
-        System.out.println(Arrays.binarySearch(values, target));
-    }
-
     public static int doBinarySearch(int[] values, int target, int start, int end) {
-
         int from = start;
         int to = end;
         int currentIndex = (start + end) / 2;
 
-        if(values[currentIndex] == target){
+        if(values[0] == target){
+            return 0;
+        }
+        if (values[currentIndex] == target) {
             return currentIndex;
         }
-        if(values[currentIndex] < target){
-            from = currentIndex + 1;
-        } else if (values[currentIndex] > target) {
-            to = currentIndex - 1;
+        if(values[start] >= values[values.length -1]) {
+            if (target < values[start] || target > values[currentIndex]) {
+                from = start + 1;
+            }
+            else {
+                to = end - 1;
+            }
+        } else {
+            if (values[currentIndex] < target) {
+                from = currentIndex + 1;
+            } else {
+                to = currentIndex - 1;
+            }
         }
         return doBinarySearch(values, target, from, to);
     }
